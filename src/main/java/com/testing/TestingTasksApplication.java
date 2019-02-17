eb795494c7cbc54bc1267d5d6a99910bc0ed9553
fin34 <p.bydolek@interia.pl>
2019-02-17 11:04:42 +0100

    Simple user

---
+++ src/main/java/com/testing/TestingTasksApplication.java
@@ -0,0 +1,13 @@
+package com.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingTasksApplication.class, args);
+    }
+
+}
