e1b57694032fd1c7d44f899801a7c50933c4c0d0
fin34 <p.bydolek@interia.pl>
2019-02-17 11:13:08 +0100

    Calculator Test

---
+++ testing/src/main/java/testing/calculator/TestingCalculator.java
@@ -0,0 +1,23 @@
+package testing.calculator;
+
+public class TestingCalculator {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 3;
+
+        if(calculator.add(a, b)==8){
+            System.out.println("Test OK");
+        }else {
+            System.out.println("Error");
+        }
+
+        if(calculator.subtract(a, b) == 2){
+            System.out.println("Test OK");
+        }else {
+            System.out.println("Error");
+        }
+    }
+}
